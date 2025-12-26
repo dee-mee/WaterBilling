2e6f0fe8803dafd270aec9d3063ce59d3ff17943
dee-mee <derekmuriuki2@gmail.com>
2025-12-26 16:53:24 +0300

    admin name change

--- create_admin.py
+++ create_admin.py
@@ -1,4 +1,4 @@
 from django.contrib.auth import get_user_model
 User = get_user_model()
-if not User.objects.filter(username='deemee').exists():
-    User.objects.create_superuser('deemee', 'deemee@gmail.com', 'Settings4$')
+if not User.objects.filter(username='deemvee').exists():
+    User.objects.create_superuser('deemvee', 'deemee@example.com', 'Settings4$')
