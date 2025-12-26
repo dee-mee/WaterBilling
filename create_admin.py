f1c1e5ecfa18bb5944e90bdec7454fd99f247849
dee-mee <derekmuriuki2@gmail.com>
2025-12-26 17:08:43 +0300

    removed credentials

--- create_admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='deemvee').exists():
-    User.objects.create_superuser('deemvee', 'deemee@example.com', 'Settings4$')
